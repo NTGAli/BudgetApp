e4333c08d8d2c546998f0753ef61b99355b90844
NTGAli <alintg@outlook.com>
2024-06-22 11:56:43 +0330

    Add spotless

--- app/src/main/java/com/ntg/budgetapp/MainActivity.kt
+++ app/src/main/java/com/ntg/budgetapp/MainActivity.kt
@@ -14,34 +14,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.ntg.budgetapp.ui.theme.BudgetAppTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            BudgetAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      BudgetAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding),
+          )
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+  Text(
+    text = "Hello $name!",
+    modifier = modifier,
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    BudgetAppTheme {
-        Greeting("Android")
-    }
+  BudgetAppTheme {
+    Greeting("Android")
+  }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.jetbrains.kotlin.android) apply false
+  alias(libs.plugins.diffplugSpotless)
+}
+
+spotless {
+  kotlin {
+    target("**/*.kt", "**/*.kts")
+    targetExclude("${layout.buildDirectory}/**/*.kt", "bin/**/*.kt", "buildSrc/**/*.kt")
+
+    ktlint()
+  }
 }
